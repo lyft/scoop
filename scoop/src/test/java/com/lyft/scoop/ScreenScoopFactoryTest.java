4f18f02ae865224181b86b47fe5d439be37dd85c
lexer <alexey.v.zaharov@gmail.com>
2016-03-28 15:37:28 -0700

    broken test

--- scoop/src/test/java/com/lyft/scoop/ScreenScoopFactoryTest.java
+++ scoop/src/test/java/com/lyft/scoop/ScreenScoopFactoryTest.java
@@ -3,26 +3,55 @@ package com.lyft.scoop;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class ScreenScoopFactoryTest {
 
+    private ScreenScoopFactory screenScoopFactory;
+    private Scoop rootScoop;
+    private ScreenScooper screenScooper;
+
+    @Before
+    public void setUp() {
+        screenScooper = new ScreenScooper();
+        screenScoopFactory = new ScreenScoopFactory(screenScooper);
+        rootScoop = new Scoop.Builder("root").build();
+    }
+
+    // [ ] - > [ A ]
     @Test
     public void createScoopForPathWhenPreviousPathWasEmpty() {
-        ScreenScoopFactory screenScoopFactory = new ScreenScoopFactory(new ScreenScooper());
 
-        Scoop rootScoop = new Scoop.Builder("root").build();
-
-        List<Screen> toPath = Arrays.<Screen>asList(new Screen1());
+        List<Screen> toPath = Arrays.<Screen>asList(new ScreenA());
 
         Scoop scoop = screenScoopFactory.createScoop(rootScoop, null, Collections.<Screen>emptyList(), toPath);
 
         assertEquals(rootScoop, scoop.getParent());
     }
 
-    static class Screen1 extends Screen {
+    // [ A ] - > [ A, B]
+    @Test
+    public void createScoopForNotEmptyPathToPathWithExtraScreen() {
+
+        List<Screen> fromPath = Arrays.<Screen>asList(new ScreenA());
+        List<Screen> toPath = Arrays.<Screen>asList(new ScreenB());
+
+        Scoop aScoop = screenScooper.createScreenScoop(new ScreenA(), rootScoop);
+
+        Scoop scoop = screenScoopFactory.createScoop(rootScoop, aScoop, fromPath, toPath);
+
+        assertEquals(aScoop, scoop.getParent());
+    }
+
+    static class ScreenA extends Screen {
+
+    }
+
+    static class ScreenB extends Screen {
 
     }
 }
