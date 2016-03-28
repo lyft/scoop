1ac87df2af78e3be62a4a481a0211f124660e799
Pierce <pjohnson@lyft.com>
2016-03-28 16:29:11 -0700

    Adding more tests for screen scoop factory

--- scoop/src/main/java/com/lyft/scoop/ScreenScoopFactory.java
+++ scoop/src/main/java/com/lyft/scoop/ScreenScoopFactory.java
@@ -11,9 +11,59 @@ public class ScreenScoopFactory {
     }
 
     public Scoop createScoop(Scoop rootScoop, Scoop currentScreenScoop, List<Screen> fromPath, List<Screen> toPath) {
+        if (toPath.isEmpty()) {
+            return rootScoop;
+        }
+        if (fromPath.isEmpty()) {
+            return screenScooper.createScreenScoop(toPath.get(toPath.size() - 1), rootScoop);
+        }
+        int index = incrementIndex(fromPath, toPath);
 
-        Scoop scoop = screenScooper.createScreenScoop(toPath.get(0), rootScoop);
+        if (index == 0) {
+            return createNewStack(rootScoop, toPath);
+        }
+        if (index >= fromPath.size()) {
+            return goForward(index, currentScreenScoop, toPath);
+        } else {
+            return goBack(index, currentScreenScoop, fromPath);
+        }
+    }
 
-        return scoop;
+    private Scoop createNewStack(Scoop rootScoop, List<Screen> toPath) {
+        Scoop nextScoop = rootScoop;
+        int index = 0;
+        while (index < toPath.size()) {
+            nextScoop = screenScooper.createScreenScoop(toPath.get(index), nextScoop);
+            index ++;
+        }
+        return nextScoop;
+    }
+
+    private int incrementIndex(List<Screen> fromPath, List<Screen> toPath) {
+        int index = 0;
+        while(index < fromPath.size() && index < toPath.size() && fromPath.get(index).equals(toPath.get(index))) {
+            index++;
+        }
+        return index;
+    }
+
+    private Scoop goForward(int index, Scoop currentScreenScoop, List<Screen> toPath) {
+        Scoop nextScoop = currentScreenScoop;
+        int count = index;
+        while (count < toPath.size()) {
+            nextScoop = screenScooper.createScreenScoop(toPath.get(count), nextScoop);
+            count ++;
+        }
+        return nextScoop;
+    }
+
+    private Scoop goBack(int index, Scoop currentScreenScoop, List<Screen> fromPath) {
+        Scoop previousScoop = currentScreenScoop;
+        int count = fromPath.size() - index;
+        while (count > 0) {
+            previousScoop = previousScoop.getParent();
+            count --;
+        }
+        return previousScoop;
     }
 }
--- scoop/src/test/java/com/lyft/scoop/ScreenScoopFactoryTest.java
+++ scoop/src/test/java/com/lyft/scoop/ScreenScoopFactoryTest.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class ScreenScoopFactoryTest {
@@ -38,7 +37,7 @@ public class ScreenScoopFactoryTest {
     public void createScoopForNotEmptyPathToPathWithExtraScreen() {
 
         List<Screen> fromPath = Arrays.<Screen>asList(new ScreenA());
-        List<Screen> toPath = Arrays.<Screen>asList(new ScreenB());
+        List<Screen> toPath = Arrays.<Screen>asList(new ScreenA(), new ScreenB());
 
         Scoop aScoop = screenScooper.createScreenScoop(new ScreenA(), rootScoop);
 
@@ -47,11 +46,74 @@ public class ScreenScoopFactoryTest {
         assertEquals(aScoop, scoop.getParent());
     }
 
+    // [ A, B ] - > [ A ]
+    @Test
+    public void createScoopForNotEmptyPathToPathWithOneLessScreen() {
+
+        List<Screen> fromPath = Arrays.<Screen>asList(new ScreenA(), new ScreenB());
+        List<Screen> toPath = Arrays.<Screen>asList(new ScreenA());
+
+        Scoop aScoop = screenScooper.createScreenScoop(new ScreenA(), rootScoop);
+        Scoop bScoop = screenScooper.createScreenScoop(new ScreenB(), aScoop);
+
+        Scoop scoop = screenScoopFactory.createScoop(rootScoop, bScoop, fromPath, toPath);
+
+        assertEquals(aScoop, scoop);
+    }
+
+    // [ A, B, C ] - > [ A ]
+    @Test
+    public void createScoopForNotEmptyPathToPathWithTwoLessScreen() {
+
+        List<Screen> fromPath = Arrays.<Screen>asList(new ScreenA(), new ScreenB(), new ScreenC());
+        List<Screen> toPath = Arrays.<Screen>asList(new ScreenA());
+
+        Scoop aScoop = screenScooper.createScreenScoop(new ScreenA(), rootScoop);
+        Scoop bScoop = screenScooper.createScreenScoop(new ScreenB(), aScoop);
+        Scoop cScoop = screenScooper.createScreenScoop(new ScreenC(), bScoop);
+
+        Scoop scoop = screenScoopFactory.createScoop(rootScoop, cScoop, fromPath, toPath);
+
+        assertEquals(aScoop, scoop);
+    }
+
+    // [ A ] - > [ A, B, C ]
+    @Test
+    public void createScoopForNotEmptyPathToPathWithTwoMoreScreen() {
+
+        List<Screen> fromPath = Arrays.<Screen>asList(new ScreenA());
+        List<Screen> toPath = Arrays.<Screen>asList(new ScreenA(), new ScreenB(), new ScreenC());
+
+        Scoop aScoop = screenScooper.createScreenScoop(new ScreenA(), rootScoop);
+
+        Scoop scoop = screenScoopFactory.createScoop(rootScoop, aScoop, fromPath, toPath);
+
+        assertEquals(aScoop, scoop.getParent().getParent());
+    }
+
+    // [ A ] - > [ B ]
+    @Test
+    public void createScoopForNotEmptyPathToDifferentScreen() {
+
+        List<Screen> fromPath = Arrays.<Screen>asList(new ScreenA());
+        List<Screen> toPath = Arrays.<Screen>asList(new ScreenB());
+
+        Scoop aScoop = screenScooper.createScreenScoop(new ScreenA(), rootScoop);
+
+        Scoop scoop = screenScoopFactory.createScoop(rootScoop, aScoop, fromPath, toPath);
+
+        assertEquals(rootScoop, scoop.getParent());
+    }
+
     static class ScreenA extends Screen {
 
     }
 
     static class ScreenB extends Screen {
+
+    }
+
+    static class ScreenC extends Screen {
 
     }
 }
