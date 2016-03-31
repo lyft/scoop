fc09931f7ee39de3e7465a1ab7cddcc68e3f5359
lexer <alexey.v.zaharov@gmail.com>
2016-03-31 13:46:46 -0700

    reimplemented screen scooper

--- scoop/src/main/java/com/lyft/scoop/ScreenScooper.java
+++ scoop/src/main/java/com/lyft/scoop/ScreenScooper.java
@@ -12,102 +12,45 @@ public class ScreenScooper {
 
     public Scoop create(Scoop rootScoop, Scoop currentScreenScoop, List<Screen> fromPath, List<Screen> toPath) {
         Scoop finalScoop = null;
+        Scoop startScoop = currentScreenScoop;
 
-        if (currentScreenScoop == null) {
+        Screen toPathFirstScreen = getFirstScreen(toPath);
+
+        while (!rootScoop.equals(startScoop) && startScoop != null) {
+            Screen scoopScreen = Screen.fromScoop(startScoop);
+
+            if (Router.sameScreen(scoopScreen, toPathFirstScreen)) {
+                break;
+            } else {
+                startScoop.destroy();
+                startScoop = startScoop.getParent();
+            }
+        }
+
+        if (rootScoop.equals(startScoop) || startScoop == null) {
             if (!toPath.isEmpty()) {
                 finalScoop = rootScoop;
 
                 for (Screen screen : toPath) {
                     finalScoop = screenScoopFactory.createScreenScoop(screen, finalScoop);
                 }
-
-                return finalScoop;
             }
-
-            return finalScoop;
-        }
-
-        int index = incrementIndex(fromPath, toPath);
-
-        return identifyRouteAndBuildScoop(index, rootScoop, currentScreenScoop, fromPath, toPath);
-    }
-
-    private Scoop identifyRouteAndBuildScoop(int index, Scoop rootScoop, Scoop currentScreenScoop, List<Screen> fromPath,
-            List<Screen> toPath) {
-        if (replaceAllWith(index)) {
-            destroyStack(currentScreenScoop, fromPath);
-            return createNewStack(rootScoop, toPath);
-        } else if (resetTo(index, fromPath, toPath)) {
-            Scoop resetScoop = goBack(index, currentScreenScoop, fromPath);
-            return goForward(index, resetScoop, toPath);
-        } else if (goTo(index, fromPath)) {
-            return goForward(index, currentScreenScoop, toPath);
         } else {
-            return goBack(index, currentScreenScoop, fromPath);
+            finalScoop = startScoop;
+            for (int i = 1; i < toPath.size(); i++) {
+                Screen screen = toPath.get(i);
+                finalScoop = screenScoopFactory.createScreenScoop(screen, finalScoop);
+            }
         }
+
+        return finalScoop;
     }
 
-    private boolean replaceAllWith(int index) {
-        return index == 0;
-    }
-
-    private boolean resetTo(int index, List<Screen> fromPath, List<Screen> toPath) {
-        return index < fromPath.size() && index < toPath.size();
-    }
-
-    private boolean goTo(int index, List<Screen> fromPath) {
-        return index >= fromPath.size();
-    }
-
-    private void destroyStack(Scoop currentScreenScoop, List<Screen> fromPath) {
-        int index = 0;
-        Scoop previousScoop = currentScreenScoop;
-        while (index < fromPath.size()) {
-            previousScoop.destroy();
-            previousScoop = previousScoop.getParent();
-            index++;
-        }
-    }
-
-    private Scoop createNewStack(Scoop rootScoop, List<Screen> toPath) {
-        if (toPath.isEmpty()) {
+    private Screen getFirstScreen(List<Screen> path) {
+        if (path.isEmpty()) {
             return null;
         }
-        Scoop nextScoop = rootScoop;
-        int index = 0;
-        while (index < toPath.size()) {
-            nextScoop = screenScoopFactory.createScreenScoop(toPath.get(index), nextScoop);
-            index++;
-        }
-        return nextScoop;
-    }
 
-    private int incrementIndex(List<Screen> fromPath, List<Screen> toPath) {
-        int index = 0;
-        while (index < fromPath.size() && index < toPath.size() && fromPath.get(index).equals(toPath.get(index))) {
-            index++;
-        }
-        return index;
-    }
-
-    private Scoop goForward(int index, Scoop scoop, List<Screen> toPath) {
-        Scoop nextScoop = scoop;
-        int count = index;
-        while (count < toPath.size()) {
-            nextScoop = screenScoopFactory.createScreenScoop(toPath.get(count), nextScoop);
-            count++;
-        }
-        return nextScoop;
-    }
-
-    private Scoop goBack(int index, Scoop scoop, List<Screen> fromPath) {
-        Scoop previousScoop = scoop;
-        int count = fromPath.size() - index;
-        while (count > 0) {
-            previousScoop.destroy();
-            previousScoop = previousScoop.getParent();
-            count--;
-        }
-        return previousScoop;
+        return path.get(0);
     }
 }
